7708c801415145d0caed16453a0113dfd1ebe18b
goodinventor <gandrews20143@gmail.com>
2019-08-15 17:24:54 -0400

    Minor change in source to test legalizeMoves().
    legalizeMoves() now returns a vector like generateMoves(). I modified the source to take this into account.

--- Cognitus.cpp
+++ Cognitus.cpp
@@ -11,13 +11,14 @@
 #include <string>
 #include <cinttypes>
 #include <vector>
+#include <algorithm>
 #include "board.h"
 #include "uci.h"
 #include "movegen.h"
 
 int main() {
 	Game *myGame = new Game;
-	const char* fen = "rnbqkbnr/ppp1pppp/8/3p4/3P4/8/PPP1PPPP/RNBQKBNR w KQkq - 0 1";
+	const char* fen = "rnb1kbnr/pppp1ppp/8/4p3/4PP1q/8/PPPP2PP/RNBQKBNR w KQkq - 0 1";
 	char* pos = _strdup(fen);
 
 	importFEN(pos, myGame); // initialize position
@@ -25,12 +26,13 @@ int main() {
 	displayBitboards();
 
 	std::vector<struct Move> myMoves = generateMoves(myGame);
-	legalizeMoves(myGame, myMoves);
 
-	for (int i = 0; i < myMoves.size(); ++i) {
-		printf(myMoves.at(i).Move[0]);
-		printf(myMoves.at(i).Move[1]);
-		printf("\n");
+	std::vector<struct Move> legalMoves = legalizeMoves(myGame, myMoves);
+
+	for (int i = 0; i < legalMoves.size(); ++i) {
+		std::printf(legalMoves.at(i).Move[0]);
+		std::printf(legalMoves.at(i).Move[1]);
+		std::printf("\n");
 	}
 
 	return 0;
